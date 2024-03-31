221bd75952dd5986c6cc4bdcfbc17c55970aec5a
Kidist Berhanu <145928754+Kindatryouts@users.noreply.github.com>
2024-03-31 18:55:34 +0300

    Method to register student

--- Driving License using Class and file/Driving License Registration using structure.cpp
+++ Driving License using Class and file/Driving License Registration using structure.cpp
@@ -31,3 +31,177 @@ short sta_month;
 void Pers_info(personal_info &p);
 void sta_date_cal(personal_info &p);
 void display(personal_info p);
+
+void Pers_info(personal_info &p)
+{
+    char check;
+    string edulevel[]={"Below grade 4","Grade 4-9","Above Grade 9"};
+
+    cout<<"\t-------------------------------------------------\n\t------Driving License School--------\n\t-------------------------------------------------\n\n";
+
+	cout<<"\t Please enter your Personal Informations\n\t----------------------------------------\n";
+
+rePNo:
+    cout<<"Phone number +251 - ";
+	cin>>p.Phone_no;
+	if(p.Phone_no.length()!=9){
+        cout<<"please enter valid number \n";
+        goto rePNo;
+        }
+
+	cout<<"Enter your Fullname : ";
+	cin.ignore();
+	getline(cin,p.Fname);
+	if(p.Fname.length()>fNamewidth)
+        fNamewidth=p.Fname.length();
+
+	cout<<"How old are you : ";
+	cin>>p.Age;
+    if(p.Age<=17 || p.Age>70)
+        goto notAVA;
+
+	cout<<"enter your level of education:\n";
+	 for(short j=0;j<3;j++){
+            cout<<"Press "<<j+1<<" for => "<<edulevel[j]<<endl;
+            }
+	cin>>p.Edu_level;
+	if(p.Edu_level==1)
+        goto notAVA;
+
+    if(p.Age>17 && p.Age<22)
+    {
+        if( p.Edu_level!=1){
+h:      cout<<"Which type of driving class do you want to take?   "<<endl;
+        for(short i=0;i<1;i++)
+		{
+             cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+        }
+		}
+        else
+            goto notAVA;
+    }
+    else if(p.Age>=22 && p.Age<24)
+    {
+        if(p.Edu_level==2){
+           goto h;
+        }
+        else if(p.Edu_level==1){
+            goto notAVA; }
+        else{
+level_1:
+        cout<<"Which type of driving class do you want to take?   "<<endl;
+        for(short i=0;i<3;i++){
+            cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+            }
+            }
+    }
+    else if(p.Age>=24 && p.Age<26)
+    {
+        if(p.Edu_level==2)
+           goto h;
+        else if(p.Edu_level==1){
+            goto notAVA; }
+        else {
+                cout<<"Do you have past experience \n Press 'Y' for yes and press any key for no : ";
+                cin>>check;
+                if(check=='Y' || check=='y')
+                {
+                    cout<<"In which type do you have exp "<<endl;
+re:                 for(short i=1;i<3;i++){
+                    cout<<"Press "<<i<<" for : "<<course_type[i]<<endl;
+                    }
+                    cin>>p.Past_exp;
+                    if(p.Past_exp==1)
+                    {
+level_2:
+                        cout<<"Which type of driving class do you want to take?    "<<endl;
+                        for(short i=0;i<4;i++){
+                        cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+                        }
+                    }
+                    else if(p.Past_exp==2)
+                    {
+level_3:
+                        cout<<"Which type of driving class do you want to take?   "<<endl;
+                        for(short i=0;i<6;i++){
+                        cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+                        }
+                    }
+                    else{
+                        cout<<"Enter 1 or 2 please \n";
+                        goto re;
+                        }
+                }
+                else
+                    goto level_1;
+
+        }
+    }
+    else if(p.Age>=26 && p.Age<=70)
+    {
+        if(p.Edu_level==2)
+           goto h;
+        else if(p.Edu_level==1){
+            goto notAVA; }
+        else {
+                cout<<"Do you have past experience \n Press 'Y' for yes and press any key for no : ";
+                cin>>check;
+                if(check=='Y' || check=='y')
+                {
+                    cout<<"In which type do you have experience "<<endl;
+re2:                for(short i=1;i<6;i++){
+                    cout<<"Press "<<i<<" for : "<<course_type[i]<<endl;
+                    }
+                    cin>>p.Past_exp;
+                    if(p.Past_exp==1)
+                    {
+                       goto level_2;
+                    }
+                    else if(p.Past_exp==2)
+                    {
+                       goto level_3;
+                    }
+                    else if(p.Past_exp==3)
+                    {
+                        cout<<"Which type of driving class do you want to take?    "<<endl;
+                        for(short i=0;i<7;i++){
+                        cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+                        }
+                    }
+                    else if(p.Past_exp==4)
+                    {
+                        cout<<"Which type of driving class do you want to take?   "<<endl;
+                        for(short i=0;i<8;i++){
+                        cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+                        }
+                    }
+                    else if(p.Past_exp==5)
+                    {
+                        cout<<"Which type of driving class do you want to take?   "<<endl;
+                        for(short i=0;i<9;i++){
+                        cout<<"Press "<<i+1<<" for : "<<course_type[i]<<endl;
+                        }
+                    }
+                    else{
+                        cout<<"Enter a number between 1 and 5 please \n";
+                        goto re2;
+                        }
+                }
+                else
+                    goto level_1;
+
+        }
+    }
+
+    else{
+notAVA:
+     system("cls");
+      cout<<"\nYou are not allowed to take driving classes.\n";
+      exit(0);
+    }
+	cin>>p.course_type;
+
+    course_no=p.course_type-1;
+cout<<"\n----------------------------------\n";
+}
+
